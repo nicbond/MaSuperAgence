1f1438684a43f45b6d14115536f6f4a43d1d7bdd
nicbond <nicolasmartins78650@free.fr>
2020-06-30 11:44:53 +0200

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class HomeController
+{
+	/**
+	* @var Environment
+	*/
+	private $twig;
+	
+	public function __construct(Environment $twig)
+    {
+		$this->twig = $twig;
+    }
+	
+    public function index(): Response
+    {
+		return New Response($this->twig->render('pages/home.html.twig'));
+    }
+}
