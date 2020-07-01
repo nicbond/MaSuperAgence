a4686e3959681fdbf0c658632857fdadb64cf0e7
nicbond <nicolasmartins78650@free.fr>
2020-07-01 15:43:03 +0200

    Add PropertyRepository directly to the function construct

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PropertyController extends AbstractController
 {
+	/**
+     * @var PropertyRepository
+    */
+	private $repository;
+	
+	public function __construct(PropertyRepository $repository)
+	{
+		$this->repository = $repository;
+	}
+	
     /**
      * @Route("/biens", name="property.index")
 	 * @return Response
      */
     public function index(): Response
     {
+		$property = $this->repository->findAllVisible();
+		dump($property[0]);
+
 		return $this->render('property/index.html.twig',  [
 			'current_menu' => 'properties']);
     }
