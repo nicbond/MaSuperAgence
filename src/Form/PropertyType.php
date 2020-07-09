50d67a226441dd34d6d29d8c44965531e1c78db5
nicbond <nicolasmartins78650@free.fr>
2020-07-09 17:08:05 +0200

    Delete created_at field from PropertyType & add french translation for name label

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -31,6 +31,7 @@ class PropertyType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Property::class,
+			'translation_domain' => 'forms'
         ]);
     }
 }
