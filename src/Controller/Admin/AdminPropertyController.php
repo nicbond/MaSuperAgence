2a3c77286df7af1928d1f59f4d9d387d17510ef2
nicbond <nicolasmartins78650@free.fr>
2020-07-09 10:20:30 +0200

    Add an edition formulaire

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Property;
+use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -39,6 +40,11 @@ class AdminPropertyController extends AbstractController
     {
 		$property = $this->repository->find($id);
 		
-		return $this->render('admin/property/edit.html.twig', compact('property'));
+		$form = $this->createForm(PropertyType::class, $property);
+		
+		return $this->render('admin/property/edit.html.twig',  [
+			'property' => $property,
+			'form' => $form->createView()
+			]);
     }
 }
