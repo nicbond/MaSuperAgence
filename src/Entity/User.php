2e1f304269c971ae0469734f6a51192407c8ddeb
nicbond <nicolasmartins78650@free.fr>
2020-07-23 15:10:24 +0200

    feat: add authentification

---
+++ src/Entity/User.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface,\Serializable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+	
+	/*
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+	{
+		return ['ROLE_ADMIN'];
+	}
+	
+	/**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+	{
+		return null;
+	}
+	
+	/**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+	{
+	}
+	
+	public function serialize()
+	{
+		return serialize([
+		   $this->id,
+		   $this->username,
+		   $this->password
+		]);
+	}
+	
+	public function unserialize($serialized)
+	{
+		list (
+		    $this->id,
+			$this->username,
+			$this->password
+			) = unserialize($serialized, ['allowed_classes' => false]);
+	}
+}
