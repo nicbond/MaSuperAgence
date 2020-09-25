11972020fff97f648e1be29edae1215665a5b587
nicbond <nicolasmartins78650@free.fr>
2020-09-25 11:32:22 +0200

    Add image filter with liip/imagine-bundle

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -40,6 +40,7 @@ class Property
 
     /**
      * @var File|null
+     * @Assert\Image(mimeTypes={"image/png", "image/jpeg", "image/jpg", "image/gif"})
      * @Vich\UploadableField(mapping="property_image", fileNameProperty="filename")
      */
     private $imageFile;
