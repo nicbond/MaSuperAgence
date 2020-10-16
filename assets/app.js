89421563c1c3ec855d4bb6a9dafe2630ee535153
nicbond <nicolasmartins78650@free.fr>
2020-10-16 13:44:14 +0200

    Add Symfony Encore webpack

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+// loads the jquery package from node_modules
+let $ = require('jquery')
+import './styles/app.css';
+require('select2')
+
+$('select').select2()
+let $contactButton = $('#contactButton')
+$contactButton.click(e => {
+  e.preventDefault();
+  $('#contactForm').slideDown();
+  $contactButton.slideUp();
+})
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/app.js');
